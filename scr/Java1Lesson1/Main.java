96327d3118f861c1beaa0b9e0d003898e8f0e987
Alexey <taler2002@gmail.com>
2018-01-22 01:14:52 +0300

    Famous "Hello World"

---
+++ scr/Java1Lesson1/Main.java
@@ -0,0 +1,7 @@
+package Java1Lesson1;
+
+public class Main {
+    public static void main(String[] args) { // генерируется psvm+Enter  Этот метод выполняется при запуске программы
+        System.out.println("Hellow, world"); // генерируется sout+Enter
+    }
+}
