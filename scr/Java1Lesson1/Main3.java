a324f3995590be2ce5768cc6dad4123c98c796a9
Alexey <taler2002@gmail.com>
2018-01-22 01:21:47 +0300

    Арифметические операции

---
+++ scr/Java1Lesson1/Main3.java
@@ -0,0 +1,22 @@
+package Java1Lesson1;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        // арифметические операции
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        //  операции присвоения
+        a+=b;
+        a-=b;
+        a*=b;
+        a/=b;
+
+        a++; //инкремент
+        a--; // декремент
+        System.out.println(a);
+    }
+}
